a610bce06ede499851ac8217147a3a28ccd971d3
PowerBroker2 <gitstuff2@gmail.com>
2022-10-13 20:54:43 -0400

    Add moments node

---
+++ Sources/armory/logicnode/MomentsNode.hx
@@ -0,0 +1,30 @@
+package armory.logicnode;
+
+import iron.math.Vec4;
+
+class MomentsNode extends LogicNode
+{
+	public function new(tree:LogicTree)
+	{
+		super(tree);
+	}
+
+	override function get(from:Int):Dynamic
+	{
+		// https://en.wikipedia.org/wiki/Drag_equation
+
+		var rho:  Float = inputs[0].get();
+		var v:    Float = inputs[1].get();
+		var Sref: Float = inputs[2].get();
+		var Cm:   Vec4  = inputs[3].get();
+		if ((rho == null) || (v == null) || (Sref == null) || (Cm == null)) return 0.0;
+
+		var moments: Vec4 = new Vec4();
+		
+		moments.x = 0.5 * rho * Math.pow(v, 2) * Sref * Cm[0];
+		moments.y = 0.5 * rho * Math.pow(v, 2) * Sref * Cm[1];
+		moments.z = 0.5 * rho * Math.pow(v, 2) * Sref * Cm[2];
+
+		return moments;
+	}
+}
---
+++ navnode_definitions/Moments.py
@@ -0,0 +1,24 @@
+import bpy
+from bpy.props import *
+from bpy.types import Node, NodeSocket
+
+from arm.logicnode.arm_nodes import *
+
+import navnode_definitions
+
+
+class MomentsNode(ArmLogicTreeNode):
+    bl_idname = 'LNMomentsNode'
+    bl_label  = 'Moments'
+
+    arm_category = navnode_definitions.CATEGORY_NAME
+    arm_section  = 'transform'
+    arm_version  = 1
+
+    def arm_init(self, context):
+        self.add_input('ArmFloatSocket', 'Density (kg/m^3)')
+        self.add_input('ArmFloatSocket', 'Velocity (m/s)')
+        self.add_input('ArmFloatSocket', 'Ref Area (m^2)')
+        self.add_input('ArmVectorSocket', 'Cf')
+
+        self.add_output('ArmVectorSocket', 'Moments (Nm)', is_var=True)
